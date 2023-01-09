7c8e7f92f5055deb2a132e3ff1f21fca22ab80ef
Quang <quangcrazymen@gmail.com>
2023-01-09 09:23:19 +0700

    Update

--- wecode/2-backtracking/Bai7.cpp
+++ wecode/2-backtracking/Bai7.cpp
@@ -6,22 +6,34 @@ using namespace std;
 
 // Get subvector from vector: https://stackoverflow.com/questions/421573/best-way-to-extract-a-subvector-from-a-vector
 // Get the last element address in vector: https://stackoverflow.com/questions/3651847/prettier-syntax-for-pointer-to-last-element-stdvector
-void evalu(vector<int> numbers,int k){
+void evalu(vector<int> numbers,int p1, int p2){
     vector<int> firstPart= vector<int>(&numbers[0],&numbers[k]);
     vector<int> secondPart= vector<int>(&numbers[k],&numbers.back()+1);
     if(k==firstPart.size()){
         cout<<firstPart[0]<<' ';
         // return firstPart[0];
-        evalu(secondPart,k+1);
+        for(int i=1;i<secondPart.size();i++){
+            evalu(secondPart,i);
+        }
     }
     if(k==secondPart.size()){
         cout<<secondPart[0]<<' ';
         // return firstPart[0];
-        evalu(firstPart,k+1);
+        for(int i=1;i<firstPart.size();i++){
+            evalu(firstPart,i);
+        }
     }
     // eval(firstPart,1);
     // eval(secondPart,1);
 
+}
+
+void eval(vector<int> &numbers,int k,int l,int r){
+    for(int i;i<numbers.size();i++){
+        eval(numbers,i+1,l,r);
+        eval(numbers,i+1,l,r);
+        result();
+    }
 }
 
 int main(){
@@ -31,6 +43,9 @@ int main(){
     // for(int i=0;i<exp.length();i+=2){
     //     numbers.push_back(stoi(string(1,exp[i]))); 
     // }
-    vector<int>test{2,3,4,5,6,7};
-    evalu(test,1);
+    vector<int>test{2,3,4,5};
+    for(int i=1;i<6;i++){
+        evalu(test,i);
+        cout<<'\n';
+    }
 }
--- wecode/2-backtracking/Bai9.cpp
+++ wecode/2-backtracking/Bai9.cpp
@@ -1,4 +1,65 @@
 // https://www.youtube.com/watch?v=XhzHXj7wrwo
 // Skyline problem
+#include<iostream>
+#include<set>
+#include<algorithm>
+#include<vector>
 
+using namespace std;
 // priority queues
+vector<vector<int>> getSkyline(vector<vector<int>>& buildings) {
+        vector<pair<int, int>> h;
+		
+        // Convert each building into two segments.
+        for (auto b : buildings) {
+            h.push_back({b[0], -b[2]});
+            h.push_back({b[1], b[2]});
+        }
+        
+        // Sort the segments.
+        sort(h.begin(), h.end());
+        int prev = 0, cur = 0;
+        
+        multiset<int> m;
+        vector<vector<int>> res;
+        
+        m.insert(0);
+        for (auto i:h) {
+            
+            // If i.second is less than zero, then it means it is left boundary.
+            if (i.second < 0) {
+                m.insert(-i.second);
+            } else { // else it is right boundary.
+                m.erase(m.find(i.second));
+            } 
+            
+            cur = *m.rbegin();
+            
+            // If current maximum height is not equal to maximum previuous height, it is a key point.
+            if (cur != prev) {
+                res.push_back({i.first, cur});
+                prev = cur;
+            }
+        }
+        return res;
+}
+
+int main(){
+    int n;
+    cin>>n;
+    vector<vector<int>> buildings(n,(vector<int>(3,0))); 
+    for(int i=0;i<n;i++){
+        for(int j=0;j<3;j++){
+            cin>>buildings[i][j];
+        }
+    }
+    // vector<vector<int>> input{{2, 9, 10},{3, 7, 15},{5, 12, 12},{15, 20, 10},{19, 24, 8}};
+    vector<vector<int>> output= getSkyline(buildings);
+    for(auto i:output){
+        for(auto j:i){
+            cout<<j<<' ';
+        }
+        cout<<'\n';
+    }
+    return 0;
+}
